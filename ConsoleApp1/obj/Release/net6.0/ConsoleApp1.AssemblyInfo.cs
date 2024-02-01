59cd4c037447cbe0350d0d098a45684918b4d2be
unknown <Admin@CSLAB209.CSSERVER.local>
2024-02-01 19:30:24 +0800

    Arith

--- ConsoleApp1/obj/Release/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d0471e6c43a363a32e092fce5d6edea72588d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
